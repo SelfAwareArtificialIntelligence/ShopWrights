a02d57a268fb48f05432921c730d5bd64f4bf028
ajayjj7 <ajayjj7@gmail.com>
2017-09-04 00:21:25 +0530

    Base Code Files
    Base Code Files

---
+++ ShopWrights/Models/DeskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopWrights.Models
+{
+    public class DeskModel
+    {
+        public string Clip { get; set; }
+        public string Weighter { get; set; }
+        public List<string> Legs { get; set; }
+        public List<string> Wheels { get; set; }
+
+    }
+}
---
+++ ShopWrights/Models/MeasurmentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopWrights.Models
+{
+    public class MeasurmentModel
+    {
+        public Int64 Length { get; set; }
+        public Int64 Height { get; set; }
+        public Int64 Width { get; set; }
+    }
+}
